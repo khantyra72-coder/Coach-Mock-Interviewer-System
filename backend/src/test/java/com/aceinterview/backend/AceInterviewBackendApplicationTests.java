8b5d519bae9c070e73aee8c4addf79776d1735e1
Chu <chunadi566@gmail.com>
2026-08-28 23:58:02 +0630

    Add isolated test database for backend

--- backend/src/test/java/com/aceinterview/backend/AceInterviewBackendApplicationTests.java
+++ backend/src/test/java/com/aceinterview/backend/AceInterviewBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.aceinterview.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AceInterviewBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
